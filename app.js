8d227b51d36babe9c9b0df8decc5dc03d2907208
yan <yan.joli@hotmail.com>
2019-06-26 16:30:29 -0400

    error in break wall werid resolved in object

--- app.js
+++ app.js
@@ -2,23 +2,9 @@
 var canvas = document.querySelector('canvas');
 var context = canvas.getContext("2d");
 
-// Box width
-var bw = 600;
-// Box height
-var bh = 600;
-//position in pixels
-var xPix = 0;
-var yPix = 0;
-//position
-var xPos = 0;
-var yPos = 0;
-//all nodes
-var nodes = null;
-var stack = null;
-
 //FUNCTIONS
 
-//  MAZE HTMLDRAWING AND LOCATION FUNCTIONS
+//  MAZE HTML DRAWING AND LOCATION FUNCTIONS
 function drawGrid(){
     for (let i = 0; i <= this.bw; i += 40) {
         this.context.moveTo(0.5 + i, 0);
@@ -40,6 +26,8 @@ function drawGrid(){
 }
 
 function breakWall(wall){
+    console.log(wall);
+    console.log(wall.resolved);//WTHHHHHHHHHHHHHHHHHH
     switch(wall){
         case 'N':
             context.clearRect(xPix-19,yPix-21,39,2);
@@ -61,21 +49,25 @@ function moveTo(direction){
     switch(direction){
         case 'N':
             updateCurrLoc(xPos,yPos-1);
+            stack.push({x: xPos, y: yPos});
             Nodes[xPos,yPos] = 1;
             displayLocation();
         break;
         case 'S':
             updateCurrLoc(xPos, yPos+1);
+            stack.push({x: xPos, y: yPos});
             Nodes[xPos,yPos] = 1;
             displayLocation();
         break;
         case 'W':
             updateCurrLoc(xPos-1,yPos);
+            stack.push({x: xPos, y: yPos});
             Nodes[xPos,yPos] = 1;
             displayLocation();
         break;
         case 'E':
             updateCurrLoc(xPos+1, yPos);
+            stack.push({x: xPos, y: yPos});
             Nodes[xPos,yPos] = 1;
             displayLocation();
         break;
@@ -111,33 +103,44 @@ function initNodes(){
     return nodes;
 }
 
-function isVisitedTwice(nodes){
-    for(let i = 0; i < nodes.length; i++){
-        for(let j = 0; j < nodes[i].length; j++)
-            if(nodes[i,j] != 2) return false;
+function isVisitedTwice(){
+    for(let i = 0; i < Nodes.length; i++){
+        for(let j = 0; j < Nodes[i].length; j++)
+            if(Nodes[i,j] != 2) return false;
     }
     return true;
 }
 
-function findNextNode(){
-    var avail = scanAdjacentNodes();
+async function findNextNode(){
+    var avail = [];
     var count = 0;
     var possible = [];
-    avail.forEach((x,i) =>{
-        if(x){
-            count++;
-            if(i == 0) possible.push('N');
-            if(i == 1) possible.push('S');
-            if(i == 2) possible.push('W');
-            if(i == 3) possible.push('E');
+    var inBackTrack = false;
+    while(true){
+        avail = scanAdjacentNodes();
+        count = 0;
+        possible = [];
+        avail.forEach((x,i) =>{
+            if(x){
+                count++;
+                if(i == 0) possible.push(0); //north
+                else if(i == 1) possible.push(1); //south
+                else if(i == 2) possible.push(2); //west
+                else if(i == 3) possible.push(3); //east
+            }
+        });
+        if(possible.length != 0){
+            if(inBackTrack) Nodes[xPos,yPos] = 2;
+            return possible[getRandomInt(count)];
+        } else {
+            stack.pop();
+            Nodes[xPos, yPos] = 2;
+            clearLocation();
+            updateCurrLoc(stack[stack.length-1].x, stack[stack.length-1].y);
+            displayLocation();
+            await sleep(500);
         }
-    });
-    if(possible.length != 0) return possible[getRandomInt(count)];
-    popNode();
-}
-
-function popNode(){
-
+    }
 }
 
 function scanAdjacentNodes(){
@@ -146,10 +149,24 @@ function scanAdjacentNodes(){
     if(yPos+1 > 14 ) avail[1] = false;
     if(xPos-1 < 0 ) avail[2] = false;
     if(xPos+1 > 14 ) avail[3] = false;
-    if(Nodes[xPos][yPos-1] != 0) avail[0] = false;
-    if(Nodes[xPos][yPos+1] != 0) avail[1] = false;
-    if(Nodes[xPos-1][yPos] != 0) avail[2] = false;
-    if(Nodes[xPos+1][yPos] != 0) avail[3] = false;
+    avail.forEach((x,i) => {
+        if(x){
+            switch(i){
+                case 0:
+                    if(Nodes[xPos][yPos-1] != 0) avail[0] = false;
+                break;
+                case 1:
+                    if(Nodes[xPos][yPos+1] != 0) avail[1] = false;
+                break;
+                case 2:
+                    if(Nodes[xPos-1][yPos] != 0) avail[2] = false;
+                break;
+                case 3:
+                    if(Nodes[xPos+1][yPos] != 0) avail[3] = false;
+                break;
+            }
+        }
+    });
     return avail;
 }
 
@@ -168,16 +185,33 @@ function sleep(ms){
 
 
 //MAIN
-    //init
+//init
+// Box width
+var bw = 600;
+// Box height
+var bh = 600;
+//position in pixels
+var xPix = 0;
+var yPix = 0;
+//position
+var xPos = 0;
+var yPos = 0;
+
+var stack = [{x: 0, y: 14}];
+var Nodes = Array.from(Array(15),() => new Array(15));
+for(let i = 0; i < Nodes.length; i++){
+    for(let j = 0; j < Nodes[i].length; j++)
+        Nodes[i][j] = 0;
+}
+
 drawGrid();
 updateCurrLoc(0, 14);
 displayLocation();
 loopMain();
 
 async function loopMain(){
-    while(true){
-        clearLocation();
-        moveTo(breakWall(getWall()));
+    while(!isVisitedTwice()){
+        moveTo(breakWall(findNextNode()));
         await sleep(500);
     }
 }
---
+++ test.js
@@ -0,0 +1,5 @@
+var lol = [{x: 1, y: 0}];
+
+lol.push({x:50, y:2});
+
+console.log(lol[lol.length-1].x);
